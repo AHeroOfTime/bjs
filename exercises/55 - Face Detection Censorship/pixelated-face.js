944426528819a9f4a0e3a21dd3b308673b59ddcc
Aaron Rodriguez <aaroner@yahoo.com>
2020-10-20 19:14:08 -0500

    55/85

--- exercises/55 - Face Detection Censorship/pixelated-face.js
+++ exercises/55 - Face Detection Censorship/pixelated-face.js
@@ -4,30 +4,28 @@ const ctx = canvas.getContext('2d');
 const faceCanvas = document.querySelector('.face');
 const faceCtx = faceCanvas.getContext('2d');
 const faceDetector = new window.FaceDetector();
-const optionsInputs = document.querySelectorAll(
-  '.controls input[type="range"]'
-);
+const optionsInput = document.querySelectorAll('.controls input[type="range"]');
 
 const options = {
   SIZE: 10,
-  SCALE: 1.35,
+  SCALE: 1.2,
 };
 
-function handleOption(event) {
-  const { value, name } = event.currentTarget;
+function handleOption(e) {
+  const { value, name } = e.currentTarget;
   options[name] = parseFloat(value);
 }
-optionsInputs.forEach(input => input.addEventListener('input', handleOption));
 
-// Write a fucntion that will populate the users video
+optionsInput.forEach((input) => input.addEventListener('input', handleOption));
+
+// write a function that will populate the users video
 async function populateVideo() {
   const stream = await navigator.mediaDevices.getUserMedia({
     video: { width: 1280, height: 720 },
   });
   video.srcObject = stream;
   await video.play();
-  // size the canvases to be the same size as the video
-  console.log(video.videoWidth, video.videoHeight);
+  // size the canvas to be the same size as the video
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   faceCanvas.width = video.videoWidth;
@@ -36,9 +34,9 @@ async function populateVideo() {
 
 async function detect() {
   const faces = await faceDetector.detect(video);
-  // ask the browser when the next animation frame is, and tell it to run detect for us
   faces.forEach(drawFace);
   faces.forEach(censor);
+  // ask the browser when the next animation frame is, and tell it to run detect for us
   requestAnimationFrame(detect);
 }
 
@@ -57,7 +55,7 @@ function censor({ boundingBox: face }) {
   faceCtx.drawImage(
     // 5 source args
     video, // where does the source come from?
-    face.x, // where do we start the source pull from?
+    face.x, // where do we start the source pull from
     face.y,
     face.width,
     face.height,
@@ -65,23 +63,22 @@ function censor({ boundingBox: face }) {
     face.x, // where should we start drawing the x and y?
     face.y,
     options.SIZE,
-    options.SIZE
+    options.SIZE,
   );
-  // draw the small face back on, but scale up
-
+  // draw the small face back on, but scale it up
   const width = face.width * options.SCALE;
   const height = face.height * options.SCALE;
   faceCtx.drawImage(
     faceCanvas, // source
-    face.x, // where do we start the source pull from?
+    face.x, // where do we start the source pull from
     face.y,
     options.SIZE,
     options.SIZE,
-    // Drawing args
+    // drawing args
     face.x - (width - face.width) / 2,
     face.y - (height - face.height) / 2,
     width,
-    height
+    height,
   );
 }
 

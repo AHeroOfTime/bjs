f9968a2d9e4b7e6ab281cedb5add3adf345e4e84
Aaron Rodriguez <aaroner@yahoo.com>
2021-11-30 11:12:57 -0600

    82/85

--- exercises/82 - npm modules - FINISHED/index.js
+++ exercises/82 - npm modules - FINISHED/index.js
@@ -1,25 +1,31 @@
 import wait from 'waait';
+// import faker from 'faker';
 import { name } from 'faker';
 import { formatDistance, format } from 'date-fns';
 import axios from 'axios';
 import { intersection, isEqual } from 'lodash';
 import to from 'await-to-js';
 
+// console.log(`Hello ${faker.name.firstName()}`);
+// console.log(`Hello ${name.firstName()}`);
+
 const fakeNames = Array.from(
   { length: 10 },
-  () => `${name.firstName()} ${name.lastName()}`
+  () => `${name.firstName()} ${name.lastName()}`,
 );
+// console.log(fakeNames);
 
 async function go() {
   console.log('Going!');
   await wait(200);
-  console.log('ending!');
+  console.log('ending');
 }
+// go();
 
-const diff = formatDistance(new Date(), new Date(2020, 3, 4, 10, 32, 0), {
+const diff = formatDistance(new Date(), new Date(2021, 3, 4, 10, 32, 0), {
   addSuffix: true,
-}); //= > 'in about 1 hour'
-console.log(diff);
+});
+// console.log(diff);
 
 const date = new Date();
 
@@ -28,31 +34,28 @@ const formatted = format(date, `LLLL 'the' do y`);
 console.log(formatted);
 
 async function getJoke() {
-  const { data } = await axios.get('https://icanhazdadjoke.com', {
+  const { data } = await axios.get('https://icanhazdadjoke.com/', {
     headers: {
       Accept: 'application/json',
     },
   });
   console.log(data);
 }
-
-// getJoke();
+getJoke();
 
 const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const b = [5, 3, 8, 3, 7, 453, 34];
-
 const sameValues = intersection(a, b);
 console.log(sameValues);
 
-const person1 = { name: 'wes' };
-const person2 = { name: 'wes' };
-
+const person1 = { name: 'aaron' };
+const person2 = { name: 'aaron' };
 console.log(isEqual(person1, person2));
 
 function checkIfNameIsCool(firstName) {
   return new Promise(function(resolve, reject) {
-    if (firstName === 'Wes') {
-      resolve('Cool name');
+    if (firstName === 'aaron') {
+      resolve('cool name');
       return;
     }
     reject(new Error('Bad Name'));
@@ -60,7 +63,7 @@ function checkIfNameIsCool(firstName) {
 }
 
 async function checkName() {
-  const [err, successValue] = await to(checkIfNameIsCool('snickers'));
+  const [err, successValue] = await to(checkIfNameIsCool('aaron'));
   if (err) {
     // deal with it
     console.log(err);
@@ -68,5 +71,4 @@ async function checkName() {
     console.log(successValue);
   }
 }
-
 checkName();
